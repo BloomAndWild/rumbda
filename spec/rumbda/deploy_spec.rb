f08834bb3cdb31c4cf57ab08fbd5db25e63ec083
Bernat Rafales <bernat@bloomandwild.com>
2022-04-13 17:43:39 +0200

    Fix spec

--- spec/rumbda/deploy_spec.rb
+++ spec/rumbda/deploy_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Rumbda::Deploy do
     context "when the update succeeds" do
       it "updates the function code for all functions" do
         config.functions.each do |function|
-          expect(lambda_client).to receive(:update_function_code).with(function, config.image_uri, config.image_tag)
+          expect(lambda_client).to receive(:update_function_code).with(function, config.image_uri, config.image_tag, config.service_version)
         end
         subject.run
       end
